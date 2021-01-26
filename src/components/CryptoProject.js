7d3629718494b684560e4ef49cec30d0990ad890
Nicholas Kniola <nickkniola@me.com>
2021-01-26 21:34:25 +0000

    Add image to profile component

--- src/components/CryptoProject.js
+++ src/components/CryptoProject.js
@@ -14,23 +14,23 @@ const CryptoProject = () => {
                 </Col>
             </Row>
             <Row className="d-flex justify-content-center">
-            <figure className="box-shadow py-4 px-4 portfolio-description">
-                <Row className="m-0" >
-                    <Col xs={7} >
-                        <div>
-                            <h4>Crypto</h4>
-                        </div>
-                        <div>
-                            <p className="text-justify">Quickly track prices of cryptocurrencies and save favorites, with up-to-date information.</p>
-                            <p className="text-justify">JavaScript ES6 | CSS3 | HTML5 | AJAX</p>
-                        </div>
-                    </Col>
-                    <Col xs={5} className="d-flex justify-content-center">
-                        <Button color="link"><a href="https://nickkniola.github.io/crypto/" target="_blank" rel="noopener noreferrer">View Site</a></Button>
-                        <Button color="link"><a href="https://github.com/nickkniola/crypto" target="_blank" rel="noopener noreferrer">View Github</a></Button>
-                    </Col>
-                </Row>
-            </figure>
+                <figure className="box-shadow py-4 px-4 portfolio-description">
+                    <Row className="m-0" >
+                        <Col xs={7} >
+                            <div>
+                                <h4>Crypto</h4>
+                            </div>
+                            <div>
+                                <p className="text-justify">Quickly track prices of cryptocurrencies and save favorites, with up-to-date information.</p>
+                                <p className="text-justify">JavaScript ES6 | CSS3 | HTML5 | AJAX</p>
+                            </div>
+                        </Col>
+                        <Col xs={5} className="d-flex justify-content-center">
+                            <Button color="link"><a href="https://nickkniola.github.io/crypto/" target="_blank" rel="noopener noreferrer">View Site</a></Button>
+                            <Button color="link"><a href="https://github.com/nickkniola/crypto" target="_blank" rel="noopener noreferrer">View Github</a></Button>
+                        </Col>
+                    </Row>
+                </figure>
             </Row>
         </Container>
     );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,16 +7,23 @@ const Profile = (props) => {
     return (
         <div id="home-section">
             <Jumbotron className="main-background bg-light">
-                <Container >
-                    <Row className="mx-auto pl-4 pr-4 portfolio-description">
-                        <Col>
-                            <h1 className="display-4 header-font">Nicholas Kniola</h1>
-                        </Col>
-                    </Row>
-                    <Row className="mx-auto pl-4 pr-4 portfolio-description">
-                        <Col className="text-justify" >
-                            <p className="lead">Exceptionally focused and reliable full stack developer, with an emphasis in Javascript and React, eager to contribute to developing projects, with a passion for creating, attention to detail, and excellent problem solving skills. Able to work both independently or as a part of a development team. Motivated to learn, grow, and excel in the software industry.</p>
-                        </Col>
+                <Container className="pl-4 pr-4">
+                    <Row>
+                    <Col xs="12" sm="8">
+                        <Row className="mx-auto portfolio-description">
+                            <Col>
+                                <h1 className="display-4 header-font">Nicholas Kniola</h1>
+                            </Col>
+                        </Row>
+                        <Row className="mx-auto portfolio-description">
+                            <Col className="text-justify" >
+                                <p className="lead">Exceptionally focused and reliable full stack developer, with an emphasis in Javascript and React, eager to contribute to developing projects, with a passion for creating, attention to detail, and excellent problem solving skills. Able to work both independently or as a part of a development team. Motivated to learn, grow, and excel in the software industry.</p>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col xs="auto">
+                        <img className="box-shadow" src="assets/images/profile.jpg" alt="Profile" />
+                    </Col>
                     </Row>
                     <div className="second-space"></div>
                 </Container>
